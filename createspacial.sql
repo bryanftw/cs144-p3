8841086ad3c5663614332cd490fdb3fa90199631
Bryan H <ilikespam321@gmail.com>
2016-02-12 19:41:26 -0800

    fixed syntax errors in sql

--- createspacial.sql
+++ createspacial.sql
@@ -1,5 +1,12 @@
 CREATE TABLE ItemCoordinates
-    AS (SELECT ItemID, Point(Latitude, Longitude) AS Coordinates
-        FROM Items INNER JOIN Sellers ON Items.SellerID = Sellers.UserID);
+(   ItemID INT(11) NOT NULL,
+    Coordinates POINT NOT NULL,
+    PRIMARY KEY (ItemID)
+)   ENGINE=MyISAM;
 
-CREATE SPATIAL INDEX sp_index ON ItemCoordinates (g);
+INSERT INTO ItemCoordinates(ItemID, Coordinates)
+    SELECT ItemID, Point(Latitude, Longitude)
+    FROM Items INNER JOIN Sellers ON Items.SellerID = Sellers.UserID
+    WHERE Latitude IS NOT NULL AND Longitude IS NOT NULL;
+
+CREATE SPATIAL INDEX sp_index ON ItemCoordinates(Coordinates);
