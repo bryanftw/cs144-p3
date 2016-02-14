55dc56e57616e14a866049787a7ed3cf7f2890ac
Dustin Rudiger <rudy@dustley.com>
2016-02-13 21:23:45 -0800

    Fixed structure, added client

---
+++ project3-indexer/buildSQLIndex.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS ItemCoordinates
+(   ItemID INT(11) NOT NULL,
+    Coordinates POINT NOT NULL,
+    PRIMARY KEY (ItemID)
+)   ENGINE=MyISAM;
+
+INSERT INTO ItemCoordinates(ItemID, Coordinates)
+    SELECT ItemID, Point(Latitude, Longitude)
+    FROM Items INNER JOIN Sellers ON Items.SellerID = Sellers.UserID
+    WHERE Latitude IS NOT NULL AND Longitude IS NOT NULL;
+
+CREATE SPATIAL INDEX sp_index ON ItemCoordinates(Coordinates);
